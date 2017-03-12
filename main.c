98c91fd98e2ec529bafd7b1ec919387e3b31f832
Maxi <mprystupiuk@gmail.com>
2017-03-12 13:03:14 -0300

    Memoria dinámica.

--- main.c
+++ main.c
@@ -3,41 +3,50 @@
 #include <stdlib.h>
 #include <string.h>
 #define LONGEST_LINE 1000
+#define WORD_SEPARATORS "\t\n " //Separadores: Tab, nueva linea, y espacio en blanco.
 
 int amountOfWordsInFile(FILE *inputFile);
 void printArray(char *arrayWords[], int amountOfWords);
-
+void freeArray(char *arrayWords[], int amountOfWords);
 
 int main() {
 	FILE *inputFile;
 	inputFile = fopen("lorem", "r");
-	int amountOfWords= amountOfWordsInFile(inputFile);
 
+	int amountOfWords = amountOfWordsInFile(inputFile);
     char *arrayOfWords[amountOfWords];
 
     //Leo de nuevo sabiendo el tamaño del vector
     rewind(inputFile);
-	int i=0;
+	int i = 0;
 	char *cp, *bp;
 	char line[LONGEST_LINE];
 	while (fgets(line, sizeof(line), inputFile) != NULL) {
         bp = line;
 		while (1) {
-            //Separadores: Tab, nueva linea, y espacio en blanco.
-			cp = strtok(bp, "\t\n ");
+			cp = strtok(bp, WORD_SEPARATORS);
 			bp = NULL;
 			if (cp == NULL)
                 break;
 
-			arrayOfWords[i++] = cp;
+			int wordLength = strlen(cp);
+			char *ptr = malloc(wordLength);
+			int z;
+			for(z = 0; z <= wordLength-1; z++){
+				ptr[z]=cp[z];
+			}
+			arrayOfWords[i] =ptr;
+			printf("palabra: %s\n",arrayOfWords[i]);
+			i++;
 		}
 	}
 	fclose(inputFile);
 
 	bubbleSort(arrayOfWords,amountOfWords);
-	quickSort(arrayOfWords,amountOfWords);
+	//quickSort(arrayOfWords,amountOfWords);
 	printArray(arrayOfWords, amountOfWords);
- return 0;
+	freeArray(arrayOfWords, amountOfWords);
+	return 0;
 }
 
 void printArray(char *arrayOfWords[], int amountOfWords){
@@ -59,7 +68,7 @@ int amountOfWordsInFile(FILE *inputFile){
         bp = line;
 		while (1) {
             //Separadores: Tab, nueva linea, y espacio en blanco.
-			cp = strtok(bp, "\t\n ");
+			cp = strtok(bp, WORD_SEPARATORS);
 			bp = NULL;
 			if (cp == NULL)
                 break;
@@ -67,6 +76,13 @@ int amountOfWordsInFile(FILE *inputFile){
 		}
 	}
 	return amountOfWords;
+}
+
+void freeArray(char *arrayWords[], int amountOfWords){
+	int i = 0;
+	for(i = 0; i <= amountOfWords-1; i++){
+		free(arrayWords[i]);
+	}
 }
 
 
